8af1980e3dd28836f282c1a5d7631004e9c26a4c
Диляра Садыкова <dilyara.mozhet.sadykova@gmail.com>
2022-01-23 20:08:01 +0300

    Добавьте файлы проекта.

---
+++ WpfLab_19_2/Models/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLab_19_2.Models
+{
+    class Circle
+    {
+        public static double Length(double a)
+        {
+            double circumference=a * 2 * Math.PI;
+            return circumference;
+        }
+    }
+}
---
+++ WpfLab_19_2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfLab_19_2.Models;
+
+namespace WpfLab_19_2.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+        private double circumference;
+        public double Circumference
+        {
+            get => circumference;
+            set
+            {
+                circumference = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Circumference = Circle.Length(Radius);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Radius != 0)
+            {
+                return true;
+            }
+            else
+                return
+                    false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
